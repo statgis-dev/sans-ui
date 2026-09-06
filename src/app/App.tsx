61f6c62df244da9c14d347a3cd3d0d495b081229
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-09-05 20:17:32 -0500

    feat(app): gov.co bar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,12 @@ import { AppBar } from "./components/AppBar";
 
 import './App.modules.css';
 import { Footer } from "./components/Footer";
+import { GovCo } from "./components/GovCo";
 
 export const App: React.FC = () => {
     return (
         <div className="shell">
+            <GovCo />
             <AppBar />
             <div className="main"></div>
             <Footer />

78f85d02039b700dad0ec8ae2f5c1cee628b0c64
Sebástian Andrés Narváez Salcedo <snarvaezwrm@gmail.com>
2026-09-05 20:19:17 -0500

    fix(app) logo size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer: React.FC = () => {
     return (
         <div className="footer">
             <div className="footer-content">
-                <img src={theme === "dark" ? 'src/app/public/logo-negative.png' : 'src/app/public/logo.png'} height="42px" style={{ marginBottom: "10px" }} />
+                <img src={theme === "dark" ? 'src/app/public/logo-negative.png' : 'src/app/public/logo.png'} height="64px" style={{ marginBottom: "8px" }} />
                 <Text size="xs" color="neutral">Corporación Autónoma Regional del Atlántico</Text>
                 <Text size="xs" color="neutral">© {formattedDate}</Text>
             </div>
